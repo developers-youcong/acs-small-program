6967a83fd43bcceb4775d3a98a6f853464d2d20a
developers-youcong <15200740587@163.com>
2019-06-13 22:29:49 +0800

    为添加支出详情增加表单校验

--- pages/detail_add/detail_add.js
+++ pages/detail_add/detail_add.js
@@ -8,16 +8,121 @@ Page({
     height: 20,
     focus: false
   },
-  bindButtonTap: function () {
-    this.setData({
-      focus: true
-    })
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：' + e.detail.value.amount + ", " + e.detail.value.typeId + ", " + this.data.remark + ", " + this.data.date + ", " + this.data.time);
+
+    var amount = e.detail.value.amount;
+    var typeId = e.detail.value.typeId;
+    var date = this.data.date;
+    var time = this.data.time;
+    var remark = this.data.remark;
+    var createDate = date + time;
+    console.log("createDate:"+createDate.toString())
+    if (amount == null || amount == "") {
+      wx.showToast({
+        title: "支出金额不能为空",
+        icon: 'none',
+        duration: 1500
+      })
+    } else if (typeId == null || typeId == "") {
+      wx.showToast({
+        title: "支出类型不能为空",
+        icon: 'none',
+        duration: 1500
+      })
+
+    } else if (date == null || date == "") {
+      wx.showToast({
+        title: "日期不能为空",
+        icon: 'none',
+        duration: 1500
+      })
+    } else if (time == null || time == "") {
+      wx.showToast({
+        title: "时间不能为空",
+        icon: 'none',
+        duration: 1500
+      })
+    } else if (remark == null || remark == "") {
+      wx.showToast({
+        title: "备注不能为空",
+        icon: 'none',
+        duration: 1500
+      })
+    } else {
+
+
+      wx.request({
+
+        url: getApp().globalData.urlPath + "spendingDetail/add",
+        method: "POST",
+        data: {
+          amount: amount,
+          typeId: typeId,
+          createDate: createDate,
+          remark: remark
+        },
+        header: {
+          "Content-Type": "application/json"
+        },
+        success: function (res) {
+          console.log(res.data.code);
+          if (res.statusCode == 200) {
+
+            //访问正常
+            if (res.data.code == "000000") {
+              wx.showToast({
+                title: "添加支出详情成功",
+                icon: 'success',
+                duration: 2000,
+                success: function () {
+
+
+
+
+
+
+                }
+              })
+
+            }
+          } else {
+
+            wx.showLoading({
+              title: '系统异常',
+              fail
+            })
+
+            setTimeout(function () {
+              wx.hideLoading()
+            }, 2000)
+          }
+
+        }
+      })
+    }
+
+
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
   },
   bindTextAreaBlur: function (e) {
-    console.log(e.detail.value)
+    this.setData({
+      remark: e.detail.value
+    })
   },
-  bindFormSubmit: function (e) {
-    console.log(e.detail.value.textarea)
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
   },
   /**
    * 生命周期函数--监听页面加载
--- pages/login/login.js
+++ pages/login/login.js
@@ -5,23 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    username:"",
-    password:""
+    username: "",
+    password: ""
 
   },
-  formSubmit: function (e) {
+  formSubmit: function(e) {
     console.log(e.detail.value.email);
     console.log(e.detail.value.pwd)
     var username = e.detail.value.email;
     var password = e.detail.value.pwd;
-    var emailReg = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/; 
-    if(username == null || username == ""){
+    var emailReg = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
+    if (username == null || username == "") {
       wx.showToast({
         title: "用户名不能为空",
         icon: 'none',
         duration: 1500
       })
-    } else if (!emailReg.test(username)){
+    } else if (!emailReg.test(username)) {
 
       wx.showToast({
         title: "邮箱有误",
@@ -29,16 +29,16 @@ Page({
         duration: 1500
       })
 
-    }else if(password == null || password == ""){
+    } else if (password == null || password == "") {
       wx.showToast({
         title: "密码不能为空",
         icon: 'none',
         duration: 1500
       })
-    }else{
+    } else {
       wx.request({
 
-        url: getApp().globalData.urlPath+"sysUser/login",
+        url: getApp().globalData.urlPath + "sysUser/login",
         method: "POST",
         data: {
           username: username,
@@ -47,17 +47,17 @@ Page({
         header: {
           "Content-Type": "application/x-www-form-urlencoded"
         },
-        success: function (res) {
+        success: function(res) {
           console.log(res.data);
           if (res.statusCode == 200) {
-           
+
             //访问正常
             if (res.data.code == "000000") {
               wx.showToast({
                 title: "登陆成功",
                 icon: 'success',
                 duration: 2000,
-                success: function () {
+                success: function() {
                   wx.navigateTo({
                     url: '../manage/manage'
                   })
@@ -75,27 +75,27 @@ Page({
                 }
               })
 
-            } else if(res.data.code == "111111") {
+            } else if (res.data.code == "111111") {
               wx.showToast({
                 title: "密码错误",
                 icon: 'none',
                 duration: 1500
               })
-            }else{
+            } else {
               wx.showToast({
                 title: "该用户不存在",
                 icon: 'none',
                 duration: 1500
               })
             }
-          }else{
+          } else {
 
             wx.showLoading({
               title: '系统异常',
               fail
             })
 
-            setTimeout(function () {
+            setTimeout(function() {
               wx.hideLoading()
             }, 2000)
           }
