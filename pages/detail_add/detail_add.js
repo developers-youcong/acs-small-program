09c24ad85d0b86ad4560b9c07255c7af3c335e48
developers-youcong <15200740587@163.com>
2019-07-05 22:41:29 +0800

    feat:添加支出详情增加下拉框

--- pages/detail_add/detail_add.js
+++ pages/detail_add/detail_add.js
@@ -5,8 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
+    selectShow: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData: ['交通', '餐饮', '生活'],//下拉列表的数据
+    index: 0,//选择的下拉列表下标
     height: 20,
     focus: false
+  },
+  selectTap() {
+    this.setData({
+      selectShow: !this.data.selectShow
+    });
+  },
+  // 点击下拉列表
+  optionTap(e) {
+    let Index = e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+ 
+    this.setData({
+      index: Index,
+      selectShow: !this.data.selectShow
+    });
   },
   formSubmit: function(e) {
     console.log('form发生了submit事件，携带数据为：' + e.detail.value.amount + ", " + e.detail.value.typeId + ", " + this.data.remark + ", " + this.data.date + ", " + this.data.time);
@@ -129,7 +146,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    var userCode = wx.getStorageSync('userId').toString();
 
+    wx.request({
+      url: getApp().globalData.urlPath + "spendingType/list",//json数据地址 
+      data: {
+        userCode: userCode,
+        pageNo: "1",
+        pageSize: "10"
+      },
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      success: function (res) {
+        console.log("msg:" + res.data.msg);
+        console.log("数据:" + res.data.datas);
+      }
+    })
   },
 
   /**
